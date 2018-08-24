35cd06497980f650f694d9c2d2f7e2a9ceeddd28
gnp005 <guru.narayana01@gmail.com>
2018-08-24 22:39:52 +0530

    first

---
+++ collapsable.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+
+  $("#item-one").click(()=>{
+    $("#list-item-one").toggleClass("none")
+  })
+
+  $("#item-two").click(()=>{
+    $("#list-item-two").toggleClass("none")
+  })
+  $("#item-three").click(()=>{
+    $("#list-item-three").toggleClass("none")
+  })
+
+})
